8d1251bcb771f669ace8407b4124f2e4c87a51e0
Krithic Kumar <krithickumarub@protonmail.com>
2023-02-27 16:48:58 +0530

    feat(core): announce every 5 seconds and reply to annoucement packets

--- crates/localsend-core/src/lib.rs
+++ crates/localsend-core/src/lib.rs
@@ -2,12 +2,14 @@ use serde::{Deserialize, Serialize};
 use std::net::Ipv4Addr;
 use std::net::UdpSocket;
 use std::str;
+use std::time::Duration;
 use uuid::Uuid;
 
 const INTERFACE_ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
 const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 167);
 const MULTICAST_PORT: u16 = 53317;
 const BUFFER_SIZE: u16 = 4096;
+pub const NUM_REPEAT: u8 = 4;
 
 const ALIAS: &str = "rustsend";
 const DEVICE_MODEL: &str = "linux";
@@ -30,25 +32,40 @@ pub struct Device {
 }
 
 impl PartialEq for Device {
+    // https://www.reddit.com/r/rust/comments/t8d6wb/comment/hznabrt
     fn eq(&self, other: &Self) -> bool {
+        // self.fingerprint == other.fingerprint && self.ip == other.ip && self.port == other.port
         self.fingerprint == other.fingerprint
     }
 }
 
 pub struct Server {
     socket: UdpSocket,
+    this_device: Device,
     devices: Vec<Device>,
-    fingerprint: uuid::Uuid,
 }
 
 impl Server {
     pub fn new() -> Self {
         let socket =
-            UdpSocket::bind((INTERFACE_ADDR, MULTICAST_PORT)).expect("couldn't bind to address'");
+            UdpSocket::bind((INTERFACE_ADDR, MULTICAST_PORT)).expect("couldn't bind to address");
+        socket
+            .set_read_timeout(Some(Duration::new(5, 0)))
+            .expect("failed to set read timeout");
         let fingerprint = Uuid::new_v4();
+        // TODO: set ip addr
+        let this_device = Device {
+            alias: ALIAS.to_string(),
+            announcement: true,
+            fingerprint: fingerprint.to_string(),
+            device_type: DEVICE_TYPE.to_string(),
+            device_model: Some(DEVICE_MODEL.to_string()),
+            ..Default::default()
+        };
+
         Self {
             socket,
-            fingerprint,
+            this_device,
             devices: vec![],
         }
     }
@@ -60,31 +77,41 @@ impl Server {
 
         let mut buf = [0u8; BUFFER_SIZE as usize];
         loop {
-            let (amt, src) = self.socket.recv_from(&mut buf).unwrap();
-            let mut data: Device = serde_json::from_slice(&buf[..amt]).unwrap();
-            data.ip = src.ip().to_string();
-            data.port = src.port();
-            if !self.devices.contains(&data) {
-                self.devices.push(data);
-                dbg!(&self.devices);
-                dbg!(&self.devices.len());
-            } else {
-                println!("{:?}", data);
+            match self.socket.recv_from(&mut buf) {
+                Ok((amt, src)) => {
+                    let mut device: Device = serde_json::from_slice(&buf[..amt]).unwrap();
+                    (device.ip, device.port) = (src.ip().to_string(), src.port());
+                    // ignore self, also note that self.this_device.ip = "", so data == this_device won't work
+                    if device.fingerprint == self.this_device.fingerprint {
+                        continue;
+                    }
+                    if device.announcement {
+                        self.announce_multicast(false);
+                    }
+
+                    match self.devices.iter().position(|dev| *dev == device) {
+                        Some(index) => {
+                            // update existing device
+                            self.devices[index] = device;
+                        }
+                        None => {
+                            // New device
+                            self.devices.push(device);
+                            dbg!(&self.devices);
+                            dbg!(&self.devices.len());
+                        }
+                    }
+                }
+                Err(_) => {
+                    self.announce_multicast(true);
+                }
             }
         }
     }
 
-    pub fn announce_multicast(&self) {
-        let device = Device {
-            alias: ALIAS.to_string(),
-            announcement: true,
-            fingerprint: self.fingerprint.to_string(),
-            device_type: DEVICE_TYPE.to_string(),
-            device_model: Some(DEVICE_MODEL.to_string()),
-            ..Default::default()
-        };
-
-        let announcement_msg = serde_json::to_string(&device).unwrap();
+    pub fn announce_multicast(&mut self, announcement: bool) {
+        self.this_device.announcement = announcement;
+        let announcement_msg = serde_json::to_string(&self.this_device).unwrap();
         self.socket
             .send_to(
                 &announcement_msg.as_bytes(),
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use localsend_core::Server;
 
 fn main() {
     let mut server = Server::new();
-    server.announce_multicast();
+    // https://github.com/localsend/protocol/issues/1#issuecomment-1426998509
+    for _ in 0..localsend_core::NUM_REPEAT {
+        server.announce_multicast(true);
+    }
     server.listen_multicast_annoucement();
 }
