cac962c23460a6f60a725230e0278b1320f3d821
Krithic Kumar <krithickumarub@protonmail.com>
2023-03-05 18:29:24 +0530

    refactor(cli): clean up code by moving logging code to functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
 use axum::{
-    extract::RawForm,
+    extract::RawBody,
     response::Html,
     routing::{get, post},
     Router,
 };
 use axum_server::tls_rustls::RustlsConfig;
 use localsend_core::Server;
+use log::{Metadata, Record};
 use rcgen::Certificate;
 use std::net::SocketAddr;
-use tracing::info;
-use tracing::Level;
+use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::FmtSubscriber;
+
+struct MyLogger;
+
+impl log::Log for MyLogger {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        false
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!(
+                "{:?}:{:?} - {} - {}",
+                record.file(),
+                record.line(),
+                record.level(),
+                record.args()
+            );
+        }
+    }
+    fn flush(&self) {}
+}
 
 #[tokio::main]
 async fn main() {
-    // let subscriber = FmtSubscriber::new();
-    // TODO: use env filter
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::DEBUG)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    init_logger(log::LevelFilter::Debug);
+    init_tracing_logger(LevelFilter::DEBUG);
 
     let cert = generate_tls_cert();
     let cert_pem = cert.serialize_pem().unwrap();
@@ -36,10 +53,7 @@ async fn main() {
         .route("/", get(handler))
         .route("/api/localsend/v1/send-request", post(send_request));
 
-    let addr = SocketAddr::from((
-        localsend_core::INTERFACE_ADDR,
-        localsend_core::MULTICAST_PORT,
-    ));
+    let addr = SocketAddr::from(([0, 0, 0, 0], localsend_core::MULTICAST_PORT));
     info!("listening on {}", addr);
     axum_server::bind_rustls(addr, config)
         .serve(app.into_make_service())
@@ -50,27 +64,53 @@ async fn main() {
 async fn announce_multicast() {
     let mut server = Server::new();
     server.listen_and_announce_multicast();
-    // https://github.com/localsend/protocol/issues/1#issuecomment-1426998509
-    // for _ in 0..localsend_core::NUM_REPEAT {
-    //     server.announce_multicast(true);
-    // }
 }
 
-async fn send_request(RawForm(form): RawForm) -> Html<&'static str> {
+async fn send_request(RawBody(form): RawBody) -> &'static str {
     info!("got request {:?}", form);
-    Html("<h1>Hello from send-request</h1>")
+    r#"{"some file id": "some token",  "another file id": "some other token"}"#
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
 }
 
+fn init_tracing_logger(level: LevelFilter) {
+    // TODO: use env filter
+    // let subscriber = FmtSubscriber::new();
+    let subscriber = FmtSubscriber::builder().with_max_level(level).finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+}
+
+fn init_logger(level: log::LevelFilter) {
+    static MY_LOGGER: MyLogger = MyLogger;
+    log::set_logger(&MY_LOGGER).unwrap();
+    log::set_max_level(level);
+}
+
 fn generate_tls_cert() -> Certificate {
     use rcgen::{CertificateParams, DnType, DnValue};
-    let mut params: CertificateParams = Default::default();
+    // let mut params: CertificateParams = Default::default();
+    let mut params =
+        CertificateParams::new(vec!["localsend.rs".to_string(), "localhost".to_string()]);
     params.distinguished_name.push(
         DnType::CommonName,
         DnValue::PrintableString("Localsend client".to_string()),
     );
+    params
+        .distinguished_name
+        .push(DnType::OrganizationName, "".to_string());
+    params
+        .distinguished_name
+        .push(DnType::OrganizationalUnitName, "".to_string());
+    params
+        .distinguished_name
+        .push(DnType::LocalityName, "".to_string());
+    params
+        .distinguished_name
+        .push(DnType::StateOrProvinceName, "".to_string());
+    params
+        .distinguished_name
+        .push(DnType::CountryName, "".to_string());
     Certificate::from_params(params).unwrap()
 }
