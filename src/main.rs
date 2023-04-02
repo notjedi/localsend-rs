6542db8e12cc2e744e2fb8c38acdf321ac2ad7be
Krithic Kumar <krithickumarub@protonmail.com>
2023-04-03 01:09:28 +0530

    remove unused feature flags and add default env filter level

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use localsend_core::{DeviceScanner, Server};
-use tracing_subscriber::{fmt::time::UtcTime, EnvFilter, FmtSubscriber};
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() {
@@ -25,14 +26,16 @@ fn start_device_scanner() {
 
 fn init_tracing_logger() {
     // TODO: use env filter
-    let time_format = time::format_description::parse("[hour]:[minute]:[second]")
-        .expect("format string should be valid!");
-    let timer = UtcTime::new(time_format);
 
     let subscriber = FmtSubscriber::builder()
-        .with_env_filter(EnvFilter::from_default_env())
+        // .with_env_filter(EnvFilter::from_default_env())
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
         .with_line_number(true)
-        .with_timer(timer)
+        .without_time()
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
