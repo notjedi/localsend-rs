628b66a6318c00731edc26e71e53e37236fec138
Krithic Kumar <krithickumarub@protonmail.com>
2023-04-07 17:01:25 +0530

    remove tui, i'm gonna make this a cli app

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use std::io;
 use std::time::Duration;
 
-use crossterm::event::{
-    self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers,
-};
-use crossterm::execute;
-use crossterm::terminal::{
-    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
-};
 use localsend_core::{ClientMessage, DeviceScanner, Server, ServerMessage};
 use tokio::runtime;
 use tokio::sync::mpsc;
@@ -15,42 +8,6 @@ use tracing::debug;
 use tracing_log::LogTracer;
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 use tracing_subscriber::FmtSubscriber;
-use tui::backend::{Backend, CrosstermBackend};
-use tui::layout::{Constraint, Direction, Layout};
-use tui::style::{Color, Modifier, Style};
-use tui::text::{Span, Spans};
-use tui::widgets::{Block, Borders, Tabs};
-use tui::{Frame, Terminal};
-
-struct App<'a> {
-    pub titles: Vec<&'a str>,
-    pub index: usize,
-}
-
-impl<'a> App<'a> {
-    fn new() -> App<'a> {
-        App {
-            titles: vec!["Receive", "Send", "About"],
-            index: 0,
-        }
-    }
-
-    pub fn next(&mut self) {
-        self.index = (self.index + 1) % self.titles.len();
-    }
-
-    pub fn previous(&mut self) {
-        if self.index > 0 {
-            self.index -= 1;
-        } else {
-            self.index = self.titles.len() - 1;
-        }
-    }
-
-    pub fn goto(&mut self, tab: usize) {
-        self.index = tab;
-    }
-}
 
 fn main() {
     init_tracing_logger();
@@ -108,95 +65,9 @@ async fn async_main() -> Result<(), io::Error> {
         }
     });
 
-    tokio::spawn(async move {
-        let server = Server::new();
-        server.start_server(server_tx, client_rx).await;
-    });
-
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    let app = App::new();
-    let res = run_app(&mut terminal, app);
-
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
-    if let Err(err) = res {
-        println!("{:?}", err);
-    }
+    let server = Server::new();
+    server.start_server(server_tx, client_rx).await;
     return Ok(());
-}
-
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-    loop {
-        terminal.draw(|f| ui(f, &app))?;
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => return Ok(()),
-                KeyCode::Char('c') => {
-                    if key.modifiers == KeyModifiers::CONTROL {
-                        return Ok(());
-                    }
-                }
-
-                KeyCode::Char('r') | KeyCode::Char('R') => app.goto(0),
-                KeyCode::Char('s') | KeyCode::Char('S') => app.goto(1),
-                KeyCode::Char('a') | KeyCode::Char('A') => app.goto(2),
-
-                KeyCode::Right | KeyCode::Char('l') => app.next(),
-                KeyCode::Left | KeyCode::Char('h') => app.previous(),
-                _ => {}
-            }
-        }
-    }
-}
-
-fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
-    let size = f.size();
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(2)
-        .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
-        .split(size);
-
-    let block = Block::default();
-    f.render_widget(block, size);
-
-    let titles = app
-        .titles
-        .iter()
-        .map(|t| {
-            let (first, rest) = t.split_at(1);
-            Spans::from(vec![
-                Span::styled(first, Style::default().fg(Color::Yellow)),
-                Span::styled(rest, Style::default().fg(Color::Green)),
-            ])
-        })
-        .collect();
-    let tabs = Tabs::new(titles)
-        .block(Block::default().borders(Borders::ALL).title("Tabs"))
-        .select(app.index)
-        .style(Style::default().fg(Color::Cyan))
-        .highlight_style(
-            Style::default()
-                .add_modifier(Modifier::BOLD)
-                .add_modifier(Modifier::ITALIC),
-        );
-    f.render_widget(tabs, chunks[0]);
-
-    let inner = Block::default().borders(Borders::ALL);
-    f.render_widget(inner, chunks[1]);
 }
 
 fn start_device_scanner() {
