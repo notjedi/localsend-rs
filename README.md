c8e5c4de82b0d618f14b217175360425d2154a48
Krithic Kumar <krithickumarub@protonmail.com>
2023-04-10 20:06:30 +0530

    refactor(bin): move sending and receiving on channel code to a separate function

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 a cli for localsend
 
-
 ## Roadmap
 
 - [x] receive files
 - [ ] send files
-- [ ] progress for sending and receiving files
+- [ ] handle connection reset errors and cancel requests when sending and receiving files
+- [ ] progress for sending files
+- [ ] pass config from bin to lib
 - [ ] config file for device name, default port, etc
 - [ ] Support protocol `v2`
 - [ ] fix `Illegal SNI hostname received` from dart side
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,12 @@ use tracing::{debug, info};
 use tracing_subscriber::filter::EnvFilter;
 use tracing_subscriber::FmtSubscriber;
 
+struct State {
+    multi_progress: MultiProgress,
+    files: HashMap<String, FileInfo>,
+    progress_map: HashMap<String, ProgressBar>,
+}
+
 fn main() {
     init_tracing_logger();
     // TODO: should i use new_current_thread or new_multi_thread?
@@ -28,78 +34,69 @@ fn main() {
     runtime.shutdown_timeout(Duration::from_millis(1));
 }
 
-async fn async_main() -> Result<(), io::Error> {
-    // spawn task to listen and announce multicast messages
-    start_device_scanner();
+async fn handle_server_msgs(
+    mut server_rx: localsend_core::protos::Receiver<ServerMessage>,
+    client_tx: localsend_core::protos::Sender<ClientMessage>,
+) {
+    // TODO: set this back to None when we are done with a session
+    let mut client_state: Option<State> = None;
 
-    let (server_tx, mut server_rx) = mpsc::unbounded_channel();
-    let (client_tx, client_rx) = mpsc::unbounded_channel();
-
-    struct State {
-        multi_progress: MultiProgress,
-        files: HashMap<String, FileInfo>,
-        progress_map: HashMap<String, ProgressBar>,
-    }
-
-    tokio::spawn(async move {
-        let mut client_state: Option<State> = None;
-
-        while let Some(server_message) = server_rx.recv().await {
-            debug!("{:?}", &server_message);
-            match server_message {
-                ServerMessage::SendFileRequest((file_id, size)) => match client_state.as_ref() {
-                    Some(state) => {
-                        state.progress_map[&file_id].inc(size as u64);
-                        if state.progress_map[&file_id].position()
-                            == (state.files[&file_id].size as u64)
-                        {
-                            state.progress_map[&file_id].finish_and_clear();
-                            state
-                                .multi_progress
-                                .println(format!("Received {}", state.files[&file_id].file_name))
-                                .unwrap();
-                        }
+    while let Some(server_message) = server_rx.recv().await {
+        debug!("{:?}", &server_message);
+        match server_message {
+            ServerMessage::SendFileRequest((file_id, size)) => match client_state.as_ref() {
+                Some(state) => {
+                    state.progress_map[&file_id].inc(size as u64);
+                    if state.progress_map[&file_id].position()
+                        == (state.files[&file_id].size as u64)
+                    {
+                        state.progress_map[&file_id].finish_and_clear();
+                        state
+                            .multi_progress
+                            .println(format!("Received {}", state.files[&file_id].file_name))
+                            .unwrap();
                     }
-                    None => {
-                        info!("client_state is None. this shouldn't be happening as this block is unreachable.")
-                    }
-                },
-                ServerMessage::SendRequest(send_request) => {
-                    println!(
-                        "{} wants to send you the following files:\n",
-                        style(send_request.device_info.alias).bold().magenta()
-                    );
+                }
+                None => {
+                    info!("client_state is None. this shouldn't be happening as this block is unreachable.")
+                }
+            },
+            ServerMessage::SendRequest(send_request) => {
+                println!(
+                    "{} wants to send you the following files:\n",
+                    style(send_request.device_info.alias).bold().magenta()
+                );
 
-                    let selections = MultiSelect::with_theme(&ColorfulTheme::default())
-                        .with_prompt("Select the files you want to receive")
-                        .items(
-                            &send_request
-                                .files
-                                .values()
-                                .map(|file_info| file_info.file_name.as_str())
-                                .collect::<Vec<&str>>(),
-                        )
-                        .defaults(&vec![true; send_request.files.len()].as_slice())
-                        .interact()
-                        .unwrap();
-
-                    if selections.is_empty() {
-                        let _ = client_tx.send(ClientMessage::Decline);
-                    } else {
-                        let file_ids = send_request
+                let selections = MultiSelect::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Select the files you want to receive")
+                    .items(
+                        &send_request
                             .files
-                            .keys()
-                            .map(|file_id| file_id.as_str())
-                            .collect::<Vec<&str>>();
+                            .values()
+                            .map(|file_info| file_info.file_name.as_str())
+                            .collect::<Vec<&str>>(),
+                    )
+                    .defaults(&vec![true; send_request.files.len()].as_slice())
+                    .interact()
+                    .unwrap();
 
-                        let selected_file_ids = selections
-                            .into_iter()
-                            .map(|idx| String::from(file_ids[idx]))
-                            .collect::<Vec<_>>();
-                        let _ = client_tx.send(ClientMessage::Allow(selected_file_ids));
+                if selections.is_empty() {
+                    let _ = client_tx.send(ClientMessage::Decline);
+                } else {
+                    let file_ids = send_request
+                        .files
+                        .keys()
+                        .map(|file_id| file_id.as_str())
+                        .collect::<Vec<&str>>();
 
-                        let multi_progress = MultiProgress::new();
-                        let progress_map = send_request
+                    let selected_file_ids = selections
+                        .into_iter()
+                        .map(|idx| String::from(file_ids[idx]))
+                        .collect::<Vec<_>>();
+                    let _ = client_tx.send(ClientMessage::Allow(selected_file_ids));
+
+                    let multi_progress = MultiProgress::new();
+                    let progress_map = send_request
                             .files
                             .clone()
                             .into_iter()
@@ -118,16 +115,25 @@ async fn async_main() -> Result<(), io::Error> {
                             })
                             .collect::<HashMap<String, ProgressBar>>();
 
-                        client_state = Some(State {
-                            files: send_request.files,
-                            multi_progress,
-                            progress_map,
-                        });
-                    }
+                    client_state = Some(State {
+                        files: send_request.files,
+                        multi_progress,
+                        progress_map,
+                    });
                 }
             }
         }
-    });
+    }
+}
+
+async fn async_main() -> Result<(), io::Error> {
+    // spawn task to listen and announce multicast messages
+    start_device_scanner();
+
+    let (server_tx, server_rx) = mpsc::unbounded_channel();
+    let (client_tx, client_rx) = mpsc::unbounded_channel();
+
+    tokio::spawn(handle_server_msgs(server_rx, client_tx));
 
     let server = Server::new();
     server.start_server(server_tx, client_rx).await;
